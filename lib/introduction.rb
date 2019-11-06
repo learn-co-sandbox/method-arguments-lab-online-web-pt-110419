6a073a708ba43838a2939e95be893a06ec6373e7
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-06 01:25:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,10 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+introduction(name)
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning #{language}."
 
 end
+introduction_with_language(name, language)
